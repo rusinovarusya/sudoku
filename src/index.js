8cfac51fac87bfed50a1508aef49d40e71e67b4c
Irina Rusinova <rusinovarusya@mail.ru>
2021-09-02 20:30:33 +0300

    feat: do 1/12 of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  const size = 9;
+
+  let matrixCopy = [];
+  for (let i = 0; i < size; ++i)
+    matrixCopy[i] = Array(size);
+
+  for (let i = 0; i < size; ++i) 
+    for (let j = 0; j < size; ++j)
+      matrixCopy[i][j] = matrix[i][j];
+
+  for (let rowIndex = 0; rowIndex < size; ++rowIndex) {
+    for (let columnIndex = 0; columnIndex < size; ++columnIndex) {
+      if (matrixCopy[rowIndex][columnIndex] == 0) {
+        //
+        let variants = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (let i = 0; i < size; ++i) {
+          for (let j = 0; j < variants.length; ++j) {
+            if (matrixCopy[rowIndex][i] == variants[j]) {
+              variants[j] = -1;
+            }
+          }
+        }
+        for (let i = 0; i < size; ++i) {
+          for (let j = 0; j < variants.length; ++j) {
+            if (matrixCopy[i][columnIndex] == variants[j]) {
+              variants[j] = -1;
+            }
+          }
+        }
+        
+        for (let i = 0; i < variants.length; ++i) {
+          if (variants[i] != -1) matrixCopy[rowIndex][columnIndex] = variants[i];
+        }
+      }
+    }
+  }
+
+  return matrixCopy;
 }
